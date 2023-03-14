1311680d5af15c030d0a3fed01837b99090a2b61
Stevdza-San <stefan.jovanavich@gmail.com>
2023-03-14 12:43:09 +0100

    Initial commit

---
+++ site/build.gradle.kts
@@ -0,0 +1,48 @@
+import com.varabyte.kobweb.gradle.application.util.configAsKobwebApplication
+
+@Suppress("DSL_SCOPE_VIOLATION") // https://youtrack.jetbrains.com/issue/KTIJ-19369
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.jetbrains.compose)
+    alias(libs.plugins.kobweb.application)
+    alias(libs.plugins.kobwebx.markdown)
+}
+
+group = "com.stevdza.san"
+version = "1.0-SNAPSHOT"
+
+kobweb {
+    app {
+        index {
+            description.set("Powered by Kobweb")
+        }
+    }
+}
+
+kotlin {
+    configAsKobwebApplication("san", includeServer = true)
+    jvmToolchain(11) // Kobweb server should use at least Java 11
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(compose.runtime)
+            }
+        }
+
+        val jsMain by getting {
+            dependencies {
+                implementation(compose.web.core)
+                implementation(libs.kobweb.core)
+                implementation(libs.kobweb.silk.core)
+                implementation(libs.kobweb.silk.icons.fa)
+                implementation(libs.kobwebx.markdown)
+             }
+        }
+        val jvmMain by getting {
+            dependencies {
+                implementation(libs.kobweb.api)
+             }
+        }
+    }
+}
