c2e2bde0d437431c2652e8ae4e8a8702ff6c512b
Stevdza-San <stefan.jovanavich@gmail.com>
2023-03-15 16:55:55 +0100

    Added Header style

--- site/src/jsMain/kotlin/com/stevdza/san/components/Header.kt
+++ site/src/jsMain/kotlin/com/stevdza/san/components/Header.kt
@@ -3,9 +3,12 @@ package com.stevdza.san.components
 import androidx.compose.runtime.Composable
 import com.stevdza.san.models.Section
 import com.stevdza.san.models.Theme
+import com.stevdza.san.styles.LogoStyle
+import com.stevdza.san.styles.NavigationItemStyle
 import com.stevdza.san.util.Constants.FONT_FAMILY
 import com.stevdza.san.util.Res
 import com.varabyte.kobweb.compose.css.FontWeight
+import com.varabyte.kobweb.compose.css.TextDecorationLine
 import com.varabyte.kobweb.compose.foundation.layout.Arrangement
 import com.varabyte.kobweb.compose.foundation.layout.Row
 import com.varabyte.kobweb.compose.ui.Alignment
@@ -13,6 +16,7 @@ import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.varabyte.kobweb.silk.components.graphics.Image
 import com.varabyte.kobweb.silk.components.navigation.Link
+import com.varabyte.kobweb.silk.components.style.toModifier
 import org.jetbrains.compose.web.css.percent
 import org.jetbrains.compose.web.css.px
 
@@ -34,6 +38,7 @@ fun Header() {
 fun LeftSide() {
     Row {
         Image(
+            modifier = LogoStyle.toModifier(),
             src = Res.Image.logo,
             desc = "Logo Image"
         )
@@ -52,11 +57,12 @@ fun RightSide() {
     ) {
         Section.values().take(6).forEach { section ->
             Link(
-                modifier = Modifier
+                modifier = NavigationItemStyle.toModifier()
                     .padding(right = 30.px)
                     .fontFamily(FONT_FAMILY)
                     .fontSize(18.px)
-                    .fontWeight(FontWeight.Normal),
+                    .fontWeight(FontWeight.Normal)
+                    .textDecorationLine(TextDecorationLine.None),
                 path = section.path,
                 text = section.title
             )
---
+++ site/src/jsMain/kotlin/com/stevdza/san/styles/MainSectionStyle.kt
@@ -0,0 +1,43 @@
+package com.stevdza.san.styles
+
+import com.stevdza.san.models.Theme
+import com.varabyte.kobweb.compose.css.CSSTransition
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.color
+import com.varabyte.kobweb.compose.ui.modifiers.transform
+import com.varabyte.kobweb.compose.ui.modifiers.transition
+import com.varabyte.kobweb.silk.components.style.ComponentStyle
+import com.varabyte.kobweb.silk.components.style.anyLink
+import com.varabyte.kobweb.silk.components.style.hover
+import org.jetbrains.compose.web.ExperimentalComposeWebApi
+import org.jetbrains.compose.web.css.deg
+import org.jetbrains.compose.web.css.ms
+
+val NavigationItemStyle by ComponentStyle {
+    base {
+        Modifier
+            .color(Theme.Secondary.rgb)
+            .transition(CSSTransition(property = "color", duration = 200.ms))
+    }
+    anyLink {
+        Modifier
+            .color(Theme.Secondary.rgb)
+    }
+    hover {
+        Modifier
+            .color(Theme.Primary.rgb)
+    }
+}
+
+@OptIn(ExperimentalComposeWebApi::class)
+val LogoStyle by ComponentStyle {
+    base {
+        Modifier
+            .transform { rotate(0.deg) }
+            .transition(CSSTransition(property = "transform", duration = 200.ms))
+    }
+    hover {
+        Modifier
+            .transform { rotate((-10).deg) }
+    }
+}
